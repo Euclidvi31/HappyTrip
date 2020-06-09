3f3ec665f3e04a7a7c3785b2e43ddf9435f541ba
Yi Shi <ysh@microsoft.com>
2020-06-09 23:02:42 +0800

    Update database

---
+++ src/Model/Migrations/20200609150215_AddTrafficLimit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTrip.Model.Migrations
+{
+    public partial class AddTrafficLimit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TrafficLimit",
+                table: "PoiHistory",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrafficLimit",
+                table: "PoiHistory");
+        }
+    }
+}
